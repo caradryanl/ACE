937a4b691ef1effb0e03751aa3d8588cd16d0331
CaradryanLiang <caradryan2022@gmail.com>
2023-09-08 16:48:23 -0700

    add target mode

--- attacks/aspl_lora_m.py
+++ attacks/aspl_lora_m.py
@@ -32,12 +32,9 @@ pynvml.nvmlInit()
 from lora_diffusion import (
     extract_lora_ups_down,
     inject_trainable_lora,
-    safetensors_available,
-    save_lora_weight,
-    save_safeloras,
 )
 from lora_diffusion.xformers_utils import set_use_memory_efficient_attention_xformers
-
+from attacks.loss import LatentAttack
 
 logger = get_logger(__name__)
 
@@ -160,37 +157,30 @@ def parse_args(input_args=None):
         help="Pretrained tokenizer name or path if not the same as model_name",
     )
     parser.add_argument(
-        "--instance_data_dir_for_train",
-        type=str,
-        default="data/n000050/set_A",
-        required=False,
-        help="A folder containing the training data of instance images.",
-    )
-    parser.add_argument(
         "--instance_data_dir_for_adversarial",
         type=str,
-        default="data/artwork-test",
+        default="data/n000050/set_B",
         required=False,
         help="A folder containing the images to add adversarial noise",
     )
     parser.add_argument(
         "--class_data_dir",
         type=str,
-        default="data/artwork-class",
+        default="data/class-person",
         required=False,
         help="A folder containing the training data of class images.",
     )
     parser.add_argument(
         "--instance_prompt",
         type=str,
-        default="a sks animation girl, masterpiece, high-quality",
+        default="a photo of sks person",
         required=False,
         help="The prompt with identifier specifying the instance",
     )
     parser.add_argument(
         "--class_prompt",
         type=str,
-        default="an animation girl, masterpiece, high-quality",
+        default="a photo of person",
         help="The prompt to specify images in the same class as provided instance images.",
     )
     parser.add_argument(
@@ -216,8 +206,8 @@ def parse_args(input_args=None):
     parser.add_argument(
         "--output_dir",
         type=str,
-        default="outputs/ASPL_M/artwork-test",
-        help="The output directory where the model predictions and checkpoints will be written.",
+        default="outputs/ASPL_M/new_version_test",
+        help="The output directory where the perturbed data is stored",
     )
     parser.add_argument("--seed", type=int, default=None, help="A seed for reproducible training.")
     parser.add_argument(
@@ -273,6 +263,18 @@ def parse_args(input_args=None):
         help="Total number of sub-steps to train adversarial noise.",
     )
     parser.add_argument(
+        "--pre_attack_steps",
+        type=int,
+        default=10,
+        help="Total number of sub-steps to train adversarial noise.",
+    )
+    parser.add_argument(
+        "--gradient_accumulation_steps",
+        type=int,
+        default=25,
+        help="Number of updates steps to accumulate before performing a backward/update pass.",
+    )
+    parser.add_argument(
         "--checkpointing_iterations",
         type=int,
         default=5,
@@ -324,18 +326,30 @@ def parse_args(input_args=None):
     parser.add_argument(
         "--pgd_alpha",
         type=float,
-        default=1e-4,
+        default=5e-3,
         help="The step size for pgd.",
     )
     parser.add_argument(
         "--pgd_eps",
         type=float,
-        default=float(5.0/255.0),
+        default=float(8.0/255.0),
         help="The noise budget for pgd.",
     )
     parser.add_argument(
+        "--pre_attack_decay",
+        type=float,
+        default=0.3,
+        help="The decay of alpha and eps when applying pre_attack",
+    )
+    parser.add_argument(
+        "--fused_weight",
+        type=float,
+        default=1e-5,
+        help="The decay of alpha and eps when applying pre_attack",
+    )
+    parser.add_argument(
         "--target_image_path",
-        default=None,
+        default="data/MIST.png",
         help="target image for attacking",
     )
 
@@ -345,12 +359,6 @@ def parse_args(input_args=None):
         type=int,
         default=500,
         help="Save checkpoint every X updates steps.",
-    )
-    parser.add_argument(
-        "--gradient_accumulation_steps",
-        type=int,
-        default=1,
-        help="Number of updates steps to accumulate before performing a backward/update pass.",
     )
     parser.add_argument(
         "--gradient_checkpointing",
@@ -666,105 +674,7 @@ def train_one_epoch(
     
     return [unet, text_encoder]
 
-class attack_mixin:
-    def __call__(
-        self,
-        latents: torch.Tensor,
-        timesteps: torch.Tensor,
-        encoder_hidden_states: torch.Tensor,
-        unet: torch.nn.Module,
-        target_tensor: torch.Tensor,
-        noise_scheduler
-    ):
-        raise NotImplementedError
-class ClassicAttack(attack_mixin):
-    """
-    This attack aims to maximize the training loss of diffusion model
-    """
-    def __call__(
-        self,
-        latents: torch.Tensor,
-        timesteps: torch.Tensor,
-        encoder_hidden_states: torch.Tensor,
-        unet: torch.nn.Module,
-        target_tensor: torch.Tensor,
-        noise_scheduler
-    ):
-        noise = torch.randn_like(latents)
-        # Add noise to the latents according to the noise magnitude at each timestep
-        # (this is the forward diffusion process)
-        noisy_latents = noise_scheduler.add_noise(latents, noise, timesteps)
-        # Predict the noise residual
-        model_pred = unet(noisy_latents, timesteps, encoder_hidden_states).sample
-        # Get the target for loss depending on the prediction type
-        if noise_scheduler.config.prediction_type == "epsilon":
-            target = noise
-        elif noise_scheduler.config.prediction_type == "v_prediction":
-            target = noise_scheduler.get_velocity(latents, noise, timesteps)
-        else:
-            raise ValueError(f"Unknown prediction type {noise_scheduler.config.prediction_type}")
-        loss = F.mse_loss(model_pred, target)
-        return loss
-class Mode1Attack(attack_mixin):
-    """
-    This attack aims to minimize the l2 distance between latent and target_tensor
-    """
-    def __call__(
-        self,
-        latents: torch.Tensor,
-        timesteps: torch.Tensor,
-        encoder_hidden_states: torch.Tensor,
-        unet: torch.nn.Module,
-        target_tensor: torch.Tensor,
-        noise_scheduler
-    ):
-        loss = F.mse_loss(latents, target_tensor, reduction="mean")
-        return loss
-class T_asplAttack(attack_mixin):
-    """
-    This attack aims to minimize l2(noise,nosie_target)
-    noise: the noise predicted by the unet, given timestep ,text_latent and latent
-    nosie_target: the noise predicted by the unet, given timestep ,text_latent and target_tensor
-    """
-    def __call__(
-        self,
-        latents: torch.Tensor,
-        timesteps: torch.Tensor,
-        encoder_hidden_states: torch.Tensor,
-        unet: torch.nn.Module,
-        target_tensor: torch.Tensor,
-        noise_scheduler
-    ):
-        noise = torch.randn_like(latents)
-        # Add noise to the latents according to the noise magnitude at each timestep
-        # (this is the forward diffusion process)
-        noisy_latents = noise_scheduler.add_noise(latents, noise, timesteps)
-        # Predict the noise residual
-        model_pred = unet(noisy_latents, timesteps, encoder_hidden_states).sample
-        # Get the target for loss depending on the prediction type
-        if noise_scheduler.config.prediction_type == "epsilon":
-            target = noise
-        elif noise_scheduler.config.prediction_type == "v_prediction":
-            target = noise_scheduler.get_velocity(latents, noise, timesteps)
-        else:
-            raise ValueError(f"Unknown prediction type {noise_scheduler.config.prediction_type}")
-        # target-shift loss
-        if target_tensor is not None:
-            xtm1_pred = torch.cat(
-                [
-                    noise_scheduler.step(
-                        model_pred[idx : idx + 1],
-                        timesteps[idx : idx + 1],
-                        noisy_latents[idx : idx + 1],
-                    ).prev_sample
-                    for idx in range(len(model_pred))
-                ]
-            )
-            xtm1_target = noise_scheduler.add_noise(target_tensor, noise, timesteps - 1)
-            loss = - F.mse_loss(xtm1_pred, xtm1_target)
-        else:
-            raise ValueError(f"target_tensor is None")
-        return loss
+
 def pgd_attack(
     args,
     models,
@@ -775,8 +685,7 @@ def pgd_attack(
     original_images: torch.Tensor,
     target_tensor: torch.Tensor,
     num_steps: int,
-    mode : str = "train",
-    accumulation_steps: int = 1,
+    mode: str = 'fused-target',
 ):
     """Return new perturbed data"""
 
@@ -791,6 +700,119 @@ def pgd_attack(
     vae.requires_grad_(False)
     text_encoder.requires_grad_(False)
     unet.requires_grad_(False)
+
+    data_tensor = data_tensor.detach().clone()
+    num_image = len(data_tensor)
+    
+    image_list = []
+    for id in range(num_image):
+        perturbed_image = data_tensor[id, :].unsqueeze(0)
+        perturbed_image.requires_grad = True
+        original_image = original_images[id, :].unsqueeze(0)
+        input_ids = tokenizer(
+            args.instance_prompt,
+            truncation=True,
+            padding="max_length",
+            max_length=tokenizer.model_max_length,
+            return_tensors="pt",
+        ).input_ids
+        input_ids = input_ids.to(device)
+        for step in range(num_steps):
+            perturbed_image.requires_grad = True
+            latents = vae.encode(perturbed_image.to(device, dtype=weight_dtype)).latent_dist.sample()
+            latents = latents * vae.config.scaling_factor
+
+            # Sample noise that we'll add to the latents
+            noise = torch.randn_like(latents)
+            bsz = latents.shape[0]
+            # Sample a random timestep for each image
+            timesteps = torch.randint(0, noise_scheduler.config.num_train_timesteps, (bsz,), device=latents.device)
+            timesteps = timesteps.long()
+            
+            # Add noise to the latents according to the noise magnitude at each timestep
+            # (this is the forward diffusion process)
+            noisy_latents = noise_scheduler.add_noise(latents, noise, timesteps)
+
+            # Get the text embedding for conditioning
+            encoder_hidden_states = text_encoder(input_ids)[0]
+
+            # Predict the noise residual
+            model_pred = unet(noisy_latents, timesteps, encoder_hidden_states).sample
+
+            # Get the target for loss depending on the prediction type
+            if noise_scheduler.config.prediction_type == "epsilon":
+                target = noise
+            elif noise_scheduler.config.prediction_type == "v_prediction":
+                target = noise_scheduler.get_velocity(latents, noise, timesteps)
+            else:
+                raise ValueError(f"Unknown prediction type {noise_scheduler.config.prediction_type}")
+
+            unet.zero_grad()
+            text_encoder.zero_grad()
+            loss = F.mse_loss(model_pred.float(), target.float(), reduction="mean")
+
+            # target-shift loss
+            if target_tensor is not None:
+                # print(model_pred.device, timesteps.device, noisy_latents.device)
+                '''
+                bug, not usable
+                xtm1_pred = torch.cat(
+                    [
+                        noise_scheduler.step(
+                            model_pred[idx : idx + 1],
+                            timesteps[idx : idx + 1],
+                            noisy_latents[idx : idx + 1],
+                        ).prev_sample
+                        for idx in range(len(model_pred))
+                    ]
+                )
+                '''
+                xtm1_pred = model_pred
+                xtm1_target = noise_scheduler.add_noise(target_tensor, noise, timesteps - 1)
+                loss = - F.mse_loss(xtm1_pred, xtm1_target)
+
+                # fused mode
+                if mode == 'fused_target':
+                    latent_attack = LatentAttack()
+                    loss = loss + args.fused_weight * latent_attack(latents, target_tensor=target_tensor)
+
+            loss = loss / args.gradient_accumulation_steps
+            loss.backward()
+            alpha = args.pgd_alpha
+            eps = args.pgd_eps
+            if step % args.gradient_accumulation_steps == args.gradient_accumulation_steps - 1:
+                adv_images = perturbed_image + alpha * perturbed_image.grad.sign()
+                eta = torch.clamp(adv_images - original_image, min=-eps, max=+eps)
+                perturbed_image = torch.clamp(original_image + eta, min=-1, max=+1).detach_()
+                perturbed_image.requires_grad = True
+            print(f"PGD loss - step {step}, loss: {loss.detach().item()}")
+
+        image_list.append(perturbed_image.detach().clone().squeeze(0))
+    outputs = torch.stack(image_list)
+
+    '''
+    handle = pynvml.nvmlDeviceGetHandleByIndex(0)
+    mem_info = pynvml.nvmlDeviceGetMemoryInfo(handle)
+    print("mem after pgd: {}".format(mem_info.used / float(1073741824)))
+    '''
+
+    return outputs
+
+
+def pre_attack(
+    args,
+    vae,
+    data_tensor: torch.Tensor,
+    original_images: torch.Tensor,
+    target_tensor: torch.Tensor,
+    num_steps: int,
+):
+    """Return new perturbed data"""
+    weight_dtype = torch.bfloat16
+    device = torch.device("cuda")
+
+    vae.to(device, dtype=weight_dtype)
+    vae.requires_grad_(False)
 
     data_tensor = data_tensor.detach().clone()
     num_image = len(data_tensor)
@@ -810,83 +832,30 @@ def pgd_attack(
         perturbed_image = data_tensor[id, :].unsqueeze(0)
         perturbed_image.requires_grad = True
         original_image = original_images[id, :].unsqueeze(0)
-        input_ids = tokenizer(
-            args.instance_prompt,
-            truncation=True,
-            padding="max_length",
-            max_length=tokenizer.model_max_length,
-            return_tensors="pt",
-        ).input_ids
         for step in range(num_steps):
             perturbed_image.requires_grad = True
             latents = vae.encode(perturbed_image.to(device, dtype=weight_dtype)).latent_dist.sample()
             latents = latents * vae.config.scaling_factor
 
-            # Sample noise that we'll add to the latents
-            noise = torch.randn_like(latents)
-            bsz = latents.shape[0]
-            # Sample a random timestep for each image
-            timesteps = torch.randint(0, noise_scheduler.config.num_train_timesteps, (bsz,), device=latents.device)
-            timesteps = timesteps.long()
-            
-            # Add noise to the latents according to the noise magnitude at each timestep
-            # (this is the forward diffusion process)
-            noisy_latents = noise_scheduler.add_noise(latents, noise, timesteps)
-
-            # Get the text embedding for conditioning
-            encoder_hidden_states = text_encoder(input_ids.to(device))[0]
-
-            # Predict the noise residual
-            model_pred = unet(noisy_latents, timesteps, encoder_hidden_states).sample
-
-            # Get the target for loss depending on the prediction type
-            if noise_scheduler.config.prediction_type == "epsilon":
-                target = noise
-            elif noise_scheduler.config.prediction_type == "v_prediction":
-                target = noise_scheduler.get_velocity(latents, noise, timesteps)
-            else:
-                raise ValueError(f"Unknown prediction type {noise_scheduler.config.prediction_type}")
-
-            unet.zero_grad()
-            text_encoder.zero_grad()
-            loss = F.mse_loss(model_pred.float(), target.float(), reduction="mean")
-
             # target-shift loss
-            if target_tensor is not None:
-                xtm1_pred = torch.cat(
-                    [
-                        noise_scheduler.step(
-                            model_pred[idx : idx + 1],
-                            timesteps[idx : idx + 1],
-                            noisy_latents[idx : idx + 1],
-                        ).prev_sample
-                        for idx in range(len(model_pred))
-                    ]
-                )
-                xtm1_target = noise_scheduler.add_noise(target_tensor, noise, timesteps - 1)
-                loss = loss - F.mse_loss(xtm1_pred, xtm1_target)
-            loss = loss / accumulation_steps
+            if target_tensor is None:
+                raise NotImplementedError("Need a targete tensor for pre-attack")
+            latent_attack = LatentAttack()
+            loss = latent_attack(latents, target_tensor=target_tensor)
+
             loss.backward()
             alpha = args.pgd_alpha
-            eps = args.pgd_eps
-            if step % accumulation_steps == accumulation_steps - 1:
-                adv_images = perturbed_image + alpha * perturbed_image.grad.sign()
-                eta = torch.clamp(adv_images - original_image, min=-eps, max=+eps)
-                perturbed_image = torch.clamp(original_image + eta, min=-1, max=+1).detach_()
-                perturbed_image.requires_grad = True
+            eps = args.pgd_eps * args.pre_attack_decay
+            adv_images = perturbed_image + alpha * perturbed_image.grad.sign()
+            eta = torch.clamp(adv_images - original_image, min=-eps, max=+eps)
+            perturbed_image = torch.clamp(original_image + eta, min=-1, max=+1).detach_()
+            perturbed_image.requires_grad = True
             print(f"PGD loss - step {step}, loss: {loss.detach().item()}")
 
         image_list.append(perturbed_image.detach().clone().squeeze(0))
     outputs = torch.stack(image_list)
 
-
-    handle = pynvml.nvmlDeviceGetHandleByIndex(0)
-    mem_info = pynvml.nvmlDeviceGetMemoryInfo(handle)
-    print("mem after pgd: {}".format(mem_info.used / float(1073741824)))
-
-
     return outputs
-
 
 def main(args):
     logging_dir = Path(args.output_dir, args.logging_dir)
@@ -1007,11 +976,6 @@ def main(args):
     if args.allow_tf32:
         torch.backends.cuda.matmul.allow_tf32 = True
 
-    clean_data = load_data(
-        args.instance_data_dir_for_train,
-        size=args.resolution,
-        center_crop=args.center_crop,
-    )
     perturbed_data = load_data(
         args.instance_data_dir_for_adversarial,
         size=args.resolution,
@@ -1053,6 +1017,15 @@ def main(args):
         vae._set_gradient_checkpointing(module=vae.encoder, value=True)
         vae._set_gradient_checkpointing(module=vae.decoder, value=True)
 
+
+    perturbed_data = pre_attack(
+        args,
+        vae,
+        perturbed_data,
+        original_data,
+        target_latent_tensor,
+        args.pre_attack_steps,
+    )
 
     f = [unet, text_encoder]
     for i in range(args.max_train_steps):
