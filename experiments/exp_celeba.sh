145392fd38e1bd4186af9e7a6f5ca7b6c9bbb987
caradryanl <caradryan2022@gmail.com>
2024-08-02 15:23:10 +0000

    fix bugs in i2i

--- experiments/exp_celeba.sh
+++ experiments/exp_celeba.sh
@@ -4,16 +4,28 @@ accelerate launch attacks/aspl_lora_m.py --instance_data_dir_for_adversarial dat
 accelerate launch attacks/aspl_lora_m.py --instance_data_dir_for_adversarial data/celeba/1499 --output_dir lora_repo/training/1499 --class_data_dir data/class/attack --instance_prompt "a photo of a sks person" --class_prompt "a photo of a person" --mixed_precision bf16 --max_train_steps 5 --checkpointing_iterations 1
 accelerate launch attacks/aspl_lora_m.py --instance_data_dir_for_adversarial data/celeba/1657 --output_dir lora_repo/training/1657 --class_data_dir data/class/attack --instance_prompt "a photo of a sks person" --class_prompt "a photo of a person" --mixed_precision bf16 --max_train_steps 5 --checkpointing_iterations 1
 
-accelerate launch train_lora_dreambooth.py --instance_data_dir lora_repo/training/121 --output_dir lora_repo/outputs/121 --class_data_dir data/class/training --output_format safe --instance_prompt "a photo of a sks person" --class_prompt "a photo of a person" --mixed_precision bf16
-accelerate launch train_lora_dreambooth.py --instance_data_dir lora_repo/training/1135 --output_dir lora_repo/outputs/1135 --class_data_dir data/class/training --output_format safe --instance_prompt "a photo of a sks person" --class_prompt "a photo of a person" --mixed_precision bf16
-accelerate launch train_lora_dreambooth.py --instance_data_dir lora_repo/training/1422 --output_dir lora_repo/outputs/1422 --class_data_dir data/class/training --output_format safe --instance_prompt "a photo of a sks person" --class_prompt "a photo of a person" --mixed_precision bf16
-accelerate launch train_lora_dreambooth.py --instance_data_dir lora_repo/training/1499 --output_dir lora_repo/outputs/1499 --class_data_dir data/class/training --output_format safe --instance_prompt "a photo of a sks person" --class_prompt "a photo of a person" --mixed_precision bf16
-accelerate launch train_lora_dreambooth.py --instance_data_dir lora_repo/training/1657 --output_dir lora_repo/outputs/1657 --class_data_dir data/class/training --output_format safe --instance_prompt "a photo of a sks person" --class_prompt "a photo of a person" --mixed_precision bf16
+accelerate launch train_lora_dreambooth.py --instance_data_dir lora_repo/training/121 --output_dir lora_repo/ace-outputs/121 --class_data_dir data/class/training --output_format safe --instance_prompt "a photo of a sks person" --class_prompt "a photo of a person" --mixed_precision bf16
+accelerate launch train_lora_dreambooth.py --instance_data_dir lora_repo/training/1135 --output_dir lora_repo/ace-outputs/1135 --class_data_dir data/class/training --output_format safe --instance_prompt "a photo of a sks person" --class_prompt "a photo of a person" --mixed_precision bf16
+accelerate launch train_lora_dreambooth.py --instance_data_dir lora_repo/training/1422 --output_dir lora_repo/ace-outputs/1422 --class_data_dir data/class/training --output_format safe --instance_prompt "a photo of a sks person" --class_prompt "a photo of a person" --mixed_precision bf16
+accelerate launch train_lora_dreambooth.py --instance_data_dir lora_repo/training/1499 --output_dir lora_repo/ace-outputs/1499 --class_data_dir data/class/training --output_format safe --instance_prompt "a photo of a sks person" --class_prompt "a photo of a person" --mixed_precision bf16
+accelerate launch train_lora_dreambooth.py --instance_data_dir lora_repo/training/1657 --output_dir lora_repo/ace-outputs/1657 --class_data_dir data/class/training --output_format safe --instance_prompt "a photo of a sks person" --class_prompt "a photo of a person" --mixed_precision bf16
 
-python i2i.py -m t2i -lp lora_repo/outputs/121/lora_weight.safetensors -op outputs/121 -spi 100 -p "a photo of a sks person"
-python i2i.py -m t2i -lp lora_repo/outputs/1135/lora_weight.safetensors -op outputs/1135 -spi 100 -p "a photo of a sks person"
-python i2i.py -m t2i -lp lora_repo/outputs/1422/lora_weight.safetensors -op outputs/1422 -spi 100 -p "a photo of a sks person"
-python i2i.py -m t2i -lp lora_repo/outputs/1499/lora_weight.safetensors -op outputs/1499 -spi 100 -p "a photo of a sks person"
-python i2i.py -m t2i -lp lora_repo/outputs/1657/lora_weight.safetensors -op outputs/1657 -spi 100 -p "a photo of a sks person"
+accelerate launch train_lora_dreambooth.py --instance_data_dir data/celeba/121 --output_dir lora_repo/clean-outputs/121 --class_data_dir data/class/training --output_format safe --instance_prompt "a photo of a sks person" --class_prompt "a photo of a person" --mixed_precision bf16
+accelerate launch train_lora_dreambooth.py --instance_data_dir data/celeba/1135 --output_dir lora_repo/clean-outputs/1135 --class_data_dir data/class/training --output_format safe --instance_prompt "a photo of a sks person" --class_prompt "a photo of a person" --mixed_precision bf16
+accelerate launch train_lora_dreambooth.py --instance_data_dir data/celeba/1422 --output_dir lora_repo/clean-outputs/1422 --class_data_dir data/class/training --output_format safe --instance_prompt "a photo of a sks person" --class_prompt "a photo of a person" --mixed_precision bf16
+accelerate launch train_lora_dreambooth.py --instance_data_dir data/celeba/1499 --output_dir lora_repo/clean-outputs/1499 --class_data_dir data/class/training --output_format safe --instance_prompt "a photo of a sks person" --class_prompt "a photo of a person" --mixed_precision bf16
+accelerate launch train_lora_dreambooth.py --instance_data_dir data/celeba/1657 --output_dir lora_repo/clean-outputs/1657 --class_data_dir data/class/training --output_format safe --instance_prompt "a photo of a sks person" --class_prompt "a photo of a person" --mixed_precision bf16
+
+python i2i.py -m t2i -lp lora_repo/clean-outputs/121/lora_weight.safetensors -op outputs/clean/121 -spi 100 -p "a photo of a sks person"
+python i2i.py -m t2i -lp lora_repo/clean-outputs/1135/lora_weight.safetensors -op outputs/clean/1135 -spi 100 -p "a photo of a sks person"
+python i2i.py -m t2i -lp lora_repo/clean-outputs/1422/lora_weight.safetensors -op outputs/clean/1422 -spi 100 -p "a photo of a sks person"
+python i2i.py -m t2i -lp lora_repo/clean-outputs/1499/lora_weight.safetensors -op outputs/clean/1499 -spi 100 -p "a photo of a sks person"
+python i2i.py -m t2i -lp lora_repo/clean-outputs/1657/lora_weight.safetensors -op outputs/clean/1657 -spi 100 -p "a photo of a sks person"
+
+python i2i.py -m t2i -lp lora_repo/ace-outputs/121/lora_weight.safetensors -op outputs/ace/121 -spi 100 -p "a photo of a sks person"
+python i2i.py -m t2i -lp lora_repo/ace-outputs/1135/lora_weight.safetensors -op outputs/ace/1135 -spi 100 -p "a photo of a sks person"
+python i2i.py -m t2i -lp lora_repo/ace-outputs/1422/lora_weight.safetensors -op outputs/ace/1422 -spi 100 -p "a photo of a sks person"
+python i2i.py -m t2i -lp lora_repo/ace-outputs/1499/lora_weight.safetensors -op outputs/ace/1499 -spi 100 -p "a photo of a sks person"
+python i2i.py -m t2i -lp lora_repo/ace-outputs/1657/lora_weight.safetensors -op outputs/ace/1657 -spi 100 -p "a photo of a sks person"
 
 
